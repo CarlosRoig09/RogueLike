19639a3d6e5cd21c1aba3349770c8d57f149fcc3
carlos.roig.7e5 <carlos.roig.7e5@itb.cat>
2022-12-13 23:50:08 +0100

    ReadmeYDiagramaDeClasses

--- Assets/Scripts/ScriptableObjectScripts/QuickLink.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "QuickLink", menuName = "QuickLink")]
-public class QuickLink : ScriptableObject
-{
-    public Player playerScript;
-    public ShootSO weaponData;
-    public ShootControler weaponScript;
-}
